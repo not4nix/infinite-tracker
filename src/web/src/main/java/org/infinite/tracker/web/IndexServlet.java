352b97e7d298143a95032738a7d06b85e24ad6dd
Viacheslav Okolitiy <justforgit@gmail.com>
2012-06-25 22:28:25 +0300

    Update project repository. Started development

---
+++ src/web/src/main/java/org/infinite/tracker/web/IndexServlet.java
@@ -0,0 +1,32 @@
+package org.infinite.tracker.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet(description = "index servlet", urlPatterns = { "/Index" })
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
